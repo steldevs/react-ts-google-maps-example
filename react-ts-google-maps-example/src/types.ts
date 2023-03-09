223936d031a7bd12fb11c40df327becde271fd68
Stel <steliscript@gmail.com>
2023-03-09 09:38:46 +0000

    remove redundant types

--- react-ts-google-maps-example/src/types.ts
+++ react-ts-google-maps-example/src/types.ts
@@ -1,28 +1,3 @@
-export type Place = {
-    addressComponents: AddressComponent[];
-    formattedAddress: string;
-    location: {
-        latitude: number;
-        longitude: number;
-    };
-    name: string;
-    placeId: string;
-    types: string[];
-}
-
-export interface AddressComponent {
-    long_name: string;
-    short_name: string;
-    types: string[];
-}
-
 export interface IGMapsApiStatus {
     status: "loading" | "idle" | "ready" | "error";
-}
-
-export interface TripRoute {
-    origin: Place | null;
-    destination: Place | null;
-    distance: number | null;
-    duration: string | null;
 }
